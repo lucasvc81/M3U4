e4c590973decf39ff7c71ddc160d6587af7e1d57
Lucas VC <lucasvc@hotmail.com>
2024-05-31 17:36:01 -0300

    Ejercicio 3

---
+++ ejercicio3.js
@@ -0,0 +1,53 @@
+var alumnos = [
+
+    {
+  
+      nombre: 'Juan Gomez',
+  
+      nota: 7
+  
+    }, {
+  
+      nombre: 'Pedro Rodriguez',
+  
+      nota: 4
+  
+    }, {
+  
+      nombre: 'Roxana García',
+  
+      nota: 8
+  
+    }, {
+  
+      nombre: 'Luciano Lopez',
+  
+      nota: 5
+  
+    }, {
+  
+      nombre: 'Fernanda Gimenez',
+  
+      nota: 6
+  
+    }, {
+  
+      nombre: 'Florencia Martinez',
+  
+      nota: 10
+  
+    }, {
+  
+      nombre: 'Raul Sanchez',
+  
+      nota: 7
+  
+    }, {
+  
+      nombre: 'Sandra Figueroa',
+  
+      nota: 8
+  
+    }
+  
+  ];
